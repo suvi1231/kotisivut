fcf164c0d2e82f51b0da6bb420ea285a80ca0f0f
Miro Tammi <mirotammi@Mac.home>
2025-02-26 15:31:55 +0200

    before henunkaa

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../images/Logodraft.png';
+import {FaInstagram , FaFacebook } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
       <div className="header-left">
-        <Link to="/">
-          <h1>Healing Naturally by Suvi</h1>
-        </Link>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            {/* <i className="fa fa-instagram" aria-hidden="true"></i> */}
+            <FaInstagram />
+          </a>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            {/* <i className="fa fa-facebook" aria-hidden="true"></i> */}
+            <FaFacebook />
+          </a>
       </div>
       <div className="header-center">
         <img src={logo} alt="Logo" />
@@ -17,16 +23,20 @@ const Header = () => {
       <div className="header-right">
         <nav>
           <ul>
-            {/* <li>
-              <Link className='linkki' to="/about">Lisätietoja</Link>
-            </li> */}
+            <li>
+              <Link className='linkki' to="/about">Lisää minusta</Link> 
+            </li>
             <li>
               <a className='button-nav' href="https://booksalon.fi/salon/healing-naturally-by-suvi?booking-state=N4IgpgtgDgNg9gTzGAkgExALhANgOwCMAxnmgMwBMAhkVQKw4AsADMxY3WAEaNFkgBfIA" target="_blank" rel="noopener noreferrer">
-                <button>Varaa aika</button>
+                <button>Ajanvaraus</button>
               </a>
             </li>
           </ul>
         </nav>
+      </div>
+      <div className="header-links">
+        <Link className="header-link" to="/">Etusivu</Link>
+        <Link className="header-link" to="/about">Yrittäjästä</Link>
       </div>
     </header>
   );
