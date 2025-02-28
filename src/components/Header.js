b3abc3199f7c2506619e84d62a40aac71790c5f9
Miro Tammi <mirotammi@Mac.home>
2025-02-28 14:49:12 +0200

    more header work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 import logo from '../images/HealingNaturally.png';
-import {FaInstagram , FaFacebook } from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const location = useLocation();
-  console.log(location.pathname)
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <header>
-    <div className='header-upper'>
-      <div className="header-left">
+      <div className='header-upper'>
+        <div className="header-left">
           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            {/* <i className="fa fa-instagram" aria-hidden="true"></i> */}
             <FaInstagram />
           </a>
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            {/* <i className="fa fa-facebook" aria-hidden="true"></i> */}
             <FaFacebook />
           </a>
+        </div>
+        <div className="header-center">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="header-right">
+          <nav>
+            <ul>
+              <li>
+                <a className='button-nav' href="https://booksalon.fi/salon/healing-naturally-by-suvi?booking-state=N4IgpgtgDgNg9gTzGAkgExALhANgOwCMAxnmgMwBMAhkVQKw4AsADMxY3WAEaNFkgBfIA" target="_blank" rel="noopener noreferrer">
+                  <button>Ajanvaraus</button>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="burger-icon" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
       </div>
-      <div className="header-center">
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className="header-right">
-        <nav>
-          <ul>
-            <li>
-              <a className='button-nav' href="https://booksalon.fi/salon/healing-naturally-by-suvi?booking-state=N4IgpgtgDgNg9gTzGAkgExALhANgOwCMAxnmgMwBMAhkVQKw4AsADMxY3WAEaNFkgBfIA" target="_blank" rel="noopener noreferrer">
-                <button>Ajanvaraus</button>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      </div>
-      <div className="header-links">
-        <Link className={`header-link ${!location.pathname.includes('minusta') ? 'active' : ''}`} to="/">Etusivu</Link>
-        <Link className={`header-link ${location.pathname.includes('minusta') ? 'active' : ''}`} to="/minusta">Yrittäjästä</Link>
+      <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+        <Link className={`header-link ${!location.pathname.includes('minusta') ? 'active' : ''}`} to="/" onClick={toggleMenu}>Etusivu</Link>
+        <Link className={`header-link ${location.pathname.includes('minusta') ? 'active' : ''}`} to="/minusta" onClick={toggleMenu}>Yrittäjästä</Link>
+        <div className="mobile-social-icons">
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
+        </div>
+        <a className='button-nav' href="https://booksalon.fi/salon/healing-naturally-by-suvi?booking-state=N4IgpgtgDgNg9gTzGAkgExALhANgOwCMAxnmgMwBMAhkVQKw4AsADMxY3WAEaNFkgBfIA" target="_blank" rel="noopener noreferrer">
+          <button>Ajanvaraus</button>
+        </a>
       </div>
     </header>
   );
